302c3b530c5db1b993ebe7ef04636ae03418937e
zerbash <dbasham@overlake.org>
2021-10-19 14:52:31 -0700

    10-19-21

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,8 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Recursion {
 
-    public static void main(String[] args) {
-        printStars(5);
-        printStarsRecursive(5);
+    public static void main(String[] args) throws FileNotFoundException {
+//        printStars(5);
+//        printStarsRecursive(5);
+//        Scanner scanner = new Scanner(new File("input.txt"));
+//        reverseLines(scanner);
+        System.out.println(pow(2,-2));
     }
 
     // Prints a line containing the given number of stars.
@@ -43,4 +50,29 @@ public class Recursion {
         }
     }
 
+    public static void reverseLines(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            reverseLines(scanner);
+            System.out.println(line);
+        }
+    }
+
+    public static double pow(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        } else if (exp % 2 == 0) {
+            return pow(base * base, exp/2);
+        } else if (exp > 0){
+            return base * pow(base, exp - 1);
+        } else {
+            return 1.0/pow(base, -exp);
+        }
+    }
+
+    public static void printBinary(int value) {
+        if (value < 2) {
+            System.out.println(value);
+        }
+    }
 }
